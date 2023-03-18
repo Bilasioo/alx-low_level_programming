6cb20705c40a553d6af19a0db7c5e86379f6874c
Bilasioo <bilasiobilly@gmail.com>
2023-03-18 02:05:18 -0700

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
 *main - my main function
-*
-*Return: 0
+*Return: always 0(success)
 */
-Int main(void)
+int main(void)
 {
-puts(“\“Programming is like building a multilingual puzzle");
-return (o);
+	puts("“Programming is like building a multilingual puzzle");
+	return (0);
 }
