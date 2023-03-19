12b296b404535aac6613b987dcf5c16411b28e6f
Bilasioo <bilasiobilly@gmail.com>
2023-03-19 04:43:43 -0700

    Write a program that prints the lowercase alphabet in reverse, followed by a new line and only use putchar twice in your code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char k;
+for (k = 'z' ; k >= 'a' ; k--)
+putchar(k);
+putchar('\n');
+return (0);
+}
