201b6fcbc750be6d4f1d7afe8a50e4a86b03cf70
Bilasioo <bilasiobilly@gmail.com>
2023-03-19 05:22:08 -0700

    Write a program that prints all possible combinations of single-digit numbers separated by ,, followed by a space. You can only use putchar four times maximum.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int k;
+
+for (k = 0; k < 10; k++)
+{
+putchar(k + '0');
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
