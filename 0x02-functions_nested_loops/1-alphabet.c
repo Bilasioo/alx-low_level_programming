0e2999fc2d8556c887281f4bf65a6a047a76d70c
Bilasioo <bilasiobilly@gmail.com>
2023-03-22 14:39:50 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *main -  print alphabet
+ * return 0
+ */
+void print_alphabet(void)
+{
+char ba;
+for (ba = 'a'; ba <= 'z'; ba++)
+	putchar(ba);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,4 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+
