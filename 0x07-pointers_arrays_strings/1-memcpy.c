6484286e6d935ad968ec89deac9b999c876996b6
Bilasioo <bilasiobilly@gmail.com>
2023-04-03 13:29:45 -0700

    MAIN_H file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,2 +1,20 @@
 #include "main.h"
-
+/**
+ * function copies n bytes from memory area src to memory area dest
+ *
+ * Returrn: dest
+ */
+int main(void)
+{
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int r = 0;
+int i = n;
+for (; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}
+}
